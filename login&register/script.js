2bada04e38d81a11cdc2fd2239ac669a014e25c9
HuyTCoder <158246228+HuyTCoder@users.noreply.github.com>
2024-02-28 11:55:06 +0700

    Add files via upload

---
+++ login&register/script.js
@@ -0,0 +1,12 @@
+document.getElementById('registerLink').addEventListener('click', function(event) {
+    event.preventDefault();
+    // Redirect to registration page
+    window.location.href = 'dangki.html';
+  });
+  
+  document.getElementById('loginLink').addEventListener('click', function(event) {
+    event.preventDefault();
+    // Redirect to login page
+    window.location.href = 'dangnhap.html';
+  });
+  
